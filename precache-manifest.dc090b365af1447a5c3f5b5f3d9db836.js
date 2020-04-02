c8b57a295c60d67500ddd63def68377de97ccc33
Reny10 <Reny10@users.noreply.github.com>
2020-04-02 23:05:20 +0100

    Updates

---
+++ precache-manifest.dc090b365af1447a5c3f5b5f3d9db836.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac21f170159cf4a1850be19a5a7ea45",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "6ee8fc357fe42efb0b1c",
+    "url": "/e-commerce/static/css/main.0f6a8ec7.chunk.css"
+  },
+  {
+    "revision": "e5e631d2c52677fdaa95",
+    "url": "/e-commerce/static/js/2.4bfabd09.chunk.js"
+  },
+  {
+    "revision": "ad61e5a3703d8af976e070e9896b739a",
+    "url": "/e-commerce/static/js/2.4bfabd09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee8fc357fe42efb0b1c",
+    "url": "/e-commerce/static/js/main.a8beb04e.chunk.js"
+  },
+  {
+    "revision": "c1a5019fc74c45d86786",
+    "url": "/e-commerce/static/js/runtime-main.68a3eb17.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce/static/media/crown.ed28accb.svg"
+  }
+]);
